a8fb83b2814f50ca0965917220bbd01b8d20bfe3
tomisuarez2 <tomisuarez2@gmail.com>
2026-02-23 22:05:12 -0300

    Plots update.

--- CalibrationTests/test_signal_characterization.py
+++ CalibrationTests/test_signal_characterization.py
@@ -9,6 +9,8 @@ import numpy as np
 import BarometricAltimeterCalibrationModules.bar_altimeter_calibration as bar
 from BarometricAltimeterCalibrationModules.utils import extract_barometric_altimeter_data, show_time_data
 
+spanish = False
+
 # Use synthetic data
 synthetic = False
 
@@ -27,11 +29,6 @@ else:
     bar_data = bar.simulate_sensor_data(60000,sampling_freq, R_real, q_real)
 n_samples = bar_data.shape[0]
 
-print(f"Number of samples in the file: {n_samples}")
-
-# Recorded data
-show_time_data(bar_data, sampling_freq, ["Relative altitude [m]", "Temperature [°C]"])
-
 rel_alt = bar_data[:,0]
 temp = bar_data[:,1]
 time_vector = np.arange(0, n_samples, 1) / sampling_freq
@@ -41,11 +38,21 @@ tau, avar = bar.compute_allan_variance(rel_alt, sampling_freq, m_steps='exponent
 rel_alt_a_dev = np.sqrt(avar).reshape(-1)
 
 # Estimate R and q values
-R, q, tauwn, taurw = bar.auto_estimate_R_q_from_allan(tau, rel_alt_a_dev, sampling_freq, plot=True)
+if spanish:
+    title_plot = "Desviacion de Allan de la medición del barómetro"
+else:
+    title_plot = "Barometer measurement Allan Deviation"
+R, q, tauwn, taurw = bar.auto_estimate_R_q_from_allan(tau, rel_alt_a_dev, sampling_freq, u='m', title=title_plot, plot=True, spanish=spanish)
 
 # Show results
-print(f"Barometric altimeter white measurement–noise variance [m²]: {R}")
-print(f"Barometric altimeter bias random–walk intensity [m²/s]: {q}")
+if spanish:
+    print(f">>> Número de muestras en el archivo de calibración: {n_samples}")
+    print(f">>> Varianza del ruido blanco de medición del barómetro [-]: {R}")
+    print(f">>> Intensidad de la caminata aleatoria del sesgo del barómetro [(-)/s]: {q}")
+else:
+    print(f">>> Number of samples in the calibration file: {n_samples}")
+    print(f">>> Barometer white measurement–noise variance [-]: {R}")
+    print(f">>> Barometer bias random–walk intensity [(-)/s]: {q}")
 
 # Save data if required
 if save:
@@ -53,6 +60,9 @@ if save:
 
 # Show time data and simulated data.
 sim_data = bar.simulate_sensor_data(n_samples, sampling_freq, R, q, temp)
-show_time_data(np.vstack([rel_alt, sim_data]).T, sampling_freq, ["Logged Signal", "Simulated Signal"])
 
+if spanish:
+    show_time_data(np.vstack([rel_alt, sim_data]).T, sampling_freq, legend=["Señal medida", "Señal simulada"], xlabel="Tiempo [s]", ylabel="[m]", title="Comparación de señales - Barómetro")
+else:
+    show_time_data(np.vstack([rel_alt, sim_data]).T, sampling_freq, ["Logged Signal", "Simulated Signal"], ylabel="[m]", title="Signal comparison - Barometer")
 
